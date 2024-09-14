d6ced7f146ff60da6bddf4e872905a3b39f6064a
Fernando <jesusfgm45@gmail.com>
2024-09-13 18:13:16 -0600

    Se agregó contenido html y se serparó mediante componentes

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+  return (
+    <header>
+        <div>
+        <h1>Little Lemon</h1>
+        <p>Chicago</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae rem, quam veritatis amet natus molestiae sunt repellendus fugit beatae minima commodi distinctio incidunt nemo blanditiis vero reiciendis nihil dolorem facilis.</p>
+        <a href='#'>Reserve a Table</a>
+        </div>
+        <img src="" alt="" />
+    </header>
+  )
+}
---
+++ src/Main.jsx
@@ -0,0 +1,56 @@
+export const Main = () => {
+  return (
+    <main>
+      <section>
+        <div>
+          <h2>This weeks specials!</h2>
+          <a href="#">Online Menu</a>
+        </div>
+        <div>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <p>Greek salad</p>
+              <span>$3.99</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis, expedita? Mollitia quo reprehenderit qui ipsam, neque
+              assumenda repellendus corporis iure nemo magni, autem consequatur
+              dolore consequuntur temporibus necessitatibus a sint?
+            </p>
+            <a href="#">Order a delivery</a>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <p>Greek salad</p>
+              <span>$3.99</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis, expedita? Mollitia quo reprehenderit qui ipsam, neque
+              assumenda repellendus corporis iure nemo magni, autem consequatur
+              dolore consequuntur temporibus necessitatibus a sint?
+            </p>
+            <a href="#">Order a delivery</a>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <div>
+              <p>Greek salad</p>
+              <span>$3.99</span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Blanditiis, expedita? Mollitia quo reprehenderit qui ipsam, neque
+              assumenda repellendus corporis iure nemo magni, autem consequatur
+              dolore consequuntur temporibus necessitatibus a sint?
+            </p>
+            <a href="#">Order a delivery</a>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+};
